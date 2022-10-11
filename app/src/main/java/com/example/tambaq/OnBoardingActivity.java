bc49ef63d2c284edb4a4c5beaf0ff6efa6a890ef
adamg <adamghirvan14@gmail.com>
2022-10-11 22:08:47 +0700

    Fix Intent

--- app/src/main/java/com/example/tambaq/OnBoardingActivity.java
+++ app/src/main/java/com/example/tambaq/OnBoardingActivity.java
@@ -4,17 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class OnBoardingActivity extends AppCompatActivity {
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding2);
 
-        Intent login = new Intent(OnBoardingActivity.this,
-                Login.class);
-        startActivity(login);
+        btnRegister = findViewById(R.id.buttonSignUp);
 
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(OnBoardingActivity.this,
+                        Login.class);
+                startActivity(login);
+            }
+        });
     }
 }
