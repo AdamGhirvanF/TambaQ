2cf2b2e999a0c845a36acfcc6b5949d622026a59
Arief Setiawan <ariefsetiawan456@gmail.com>
2022-10-11 21:49:17 +0700

    Final Commit RegisterActivity

--- app/src/main/java/com/example/tambaq/RegisterActivity.java
+++ app/src/main/java/com/example/tambaq/RegisterActivity.java
@@ -1,10 +1,10 @@
 package com.example.tambaq;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 public class RegisterActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
